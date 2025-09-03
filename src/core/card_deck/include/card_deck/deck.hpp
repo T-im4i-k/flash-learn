5d46d78d0d097a9598805fd8efc596d67ea74d76
Ivanov <ivanovtimofey1@gmail.com>
2025-09-03 11:13:08 +0200

    Add Title & Description For Deck - Add title_ & description attributes - Add methods for accessing & modifying them

--- src/core/card_deck/include/card_deck/deck.hpp
+++ src/core/card_deck/include/card_deck/deck.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <random>
+#include <string>
 #include <vector>
 
 #include "alias/alias.hpp"
@@ -12,9 +13,18 @@ namespace Core {
         using DeckIt = std::vector<Card>::iterator;
         using ConstDeckIt = std::vector<Card>::const_iterator;
 
-        explicit Deck(Int id, std::initializer_list<Card> cards_list = {});
+        explicit Deck(Int id, std::string title, std::string description,
+                      std::initializer_list<Card> cards_list = {});
 
         [[nodiscard]] Int getId() const;
+
+        [[nodiscard]] const std::string &getTitle() const;
+
+        [[nodiscard]] const std::string &getDescription() const;
+
+        void setTitle(std::string title);
+
+        void setDescription(std::string description);
 
         DeckIt erase(DeckIt it);
 
@@ -36,6 +46,8 @@ namespace Core {
 
     private:
         Int id_;
+        std::string title_;
+        std::string description_;
         std::vector<Card> cards_vector_;
         std::mt19937_64 random_engine_;
     };
--- src/core/card_deck/src/deck.cpp
+++ src/core/card_deck/src/deck.cpp
@@ -5,11 +5,23 @@
 #include "card_deck/deck.hpp"
 
 namespace Core {
-    Deck::Deck(const Int id, const std::initializer_list<Card> cards_list) :
-        id_(id), cards_vector_(cards_list),
-        random_engine_(std::random_device{}()) {}
+    Deck::Deck(const Int id, std::string title, std::string description,
+               const std::initializer_list<Card> cards_list) :
+        id_(id), title_(std::move(title)), description_(std::move(description)),
+        cards_vector_(cards_list), random_engine_(std::random_device{}()) {}
 
     Int Deck::getId() const { return id_; }
+
+    const std::string &Deck::getTitle() const { return title_; }
+
+    const std::string &Deck::getDescription() const { return description_; }
+
+    void Deck::setTitle(std::string title) { title_ = std::move(title); }
+
+    void Deck::setDescription(std::string description) {
+        description_ = std::move(description);
+    }
+
 
     Deck::DeckIt Deck::erase(const DeckIt it) {
         return cards_vector_.erase(it);

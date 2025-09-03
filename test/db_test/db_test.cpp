6f81a94010fd73c4f35cb1109e2d536ae71cc70c
Roman Remeslo <remesloroman@gmail.com>
2025-09-03 14:30:44 +0200

    Refactor db_test

--- test/db_test/db_test.cpp
+++ test/db_test/db_test.cpp
@@ -1,6 +1,5 @@
 #include <QCoreApplication>
 #include <cassert>
-#include <iostream>
 #include <queue>
 
 #include "db/core/db_manager.hpp"
@@ -8,12 +7,31 @@
 #include "db/repository/card_repository.hpp"
 #include "db/repository/deck_repository.hpp"
 
-int main(int argc, char **argv) {
+namespace TestUtils {
+    bool cardEqual(const Core::Card &lhs, const Core::Card &rhs) {
+        return lhs.id() == rhs.id() && lhs.front() == rhs.front() &&
+               lhs.back() == rhs.back();
+    }
 
+    bool deckEqual(const Core::Deck & lhs, const Core::Deck & rhs) {
+        const auto expectedSize = lhs.cardVector().size();
+
+        for (size_t i = 0; i < expectedSize; i++) {
+            if (cardEqual(lhs.cardVector()[i],rhs.cardVector()[i]))
+                return false;
+        }
+
+        return true;
+    }
+}
+
+int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
 
     DB::DBManager::open(":memory:");
     DB::SchemaInitializer::initialize();
+
+    // Add & Select tests
 
     int deck1 = DB::DeckRepository::addDeck("Spanish A2");
 
@@ -21,47 +39,54 @@ int main(int argc, char **argv) {
     assert(DB::CardRepository::addCard(deck1, "salsa", "sauce"));
     assert(DB::CardRepository::addCard(deck1, "sombrero", "hat"));
 
-    std::queue<std::string> result;
-    result.emplace("aburrido");
-    result.emplace("salsa");
-    result.emplace("sombrero");
-
+    Core::Deck result(deck1, "", "", {
+        Core::Card(-1,"aburrido", "boring"),
+        Core::Card(-1,"salsa", "sauce"),
+        Core::Card(-1,"sombrero", "hat")
+    });
 
     Core::Deck response(deck1, "", "");
     DB::CardRepository::selectCardsByDeck(response);
 
-    assert(!response.cardVector().empty());
+    assert(TestUtils::deckEqual(result, response));
 
-    for (const auto& card : response.cardVector()) {
-        assert(card.front() == result.front());
-        result.pop();
-    }
 
     int deck2 = DB::DeckRepository::addDeck("Czech A1");
 
     assert(DB::CardRepository::addCard(deck2, "taska", "bag"));
-    assert(DB::CardRepository::addCard(deck2, "dzus", "juice"));
+    int toDel = DB::CardRepository::addCard(deck2, "dzus", "juice");
     assert(DB::CardRepository::addCard(deck2, "stul", "table"));
-    assert(DB::CardRepository::addCard(deck2, "obchod", "shop"));
+    int toDel2 = DB::CardRepository::addCard(deck2, "obchod", "shop");
     assert(DB::CardRepository::addCard(deck2, "kniha", "book"));
 
-    std::vector deck2Result = {
-        std::make_pair<std::string, std::string>("taska", "bag"),
-        std::make_pair<std::string, std::string>("dzus", "juice"),
-        std::make_pair<std::string, std::string>("stul", "table"),
-        std::make_pair<std::string, std::string>("obchod", "shop"),
-        std::make_pair<std::string, std::string>("kniha", "book")
-    };
+    Core::Deck result2(deck2, "", "", {
+        Core::Card(-1,"taska", "bag"),
+        Core::Card(-1,"dzus", "juice"),
+        Core::Card(-1,"stul", "table"),
+        Core::Card(-1,"obchod", "shop"),
+        Core::Card(-1,"kniha", "book"),
+
+    });
 
     Core::Deck response2(deck2, "", "");
     DB::CardRepository::selectCardsByDeck(response2);
 
-    const auto expectedSize = deck2Result.size();
+    assert(TestUtils::deckEqual(result2, response2));
 
-    for (size_t i = 0; i < expectedSize; i++) {
-        assert(deck2Result[i].first == response2.cardVector()[i].front() &&
-               deck2Result[i].second == response2.cardVector()[i].back());
-    }
+    // Removing test
+
+    assert(DB::CardRepository::removeCard(deck2, toDel));
+    assert(DB::CardRepository::removeCard(deck2, toDel2));
+
+    Core::Deck result3(deck2, "", "", {
+        Core::Card(-1,"taska", "bag"),
+        Core::Card(-1,"stul", "table"),
+        Core::Card(-1,"kniha", "book"),
+    });
+
+    DB::CardRepository::selectCardsByDeck(response2);
+
+    assert(TestUtils::deckEqual(result3, response2));
 
     return 0;
 }
